4d2cbcec0f7355dad99b3425eb4186981807d06f
Matvdg <matvdg@me.com>
2016-05-11 19:20:40 -0400

    first commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,132 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Mathieu Vandeginste on 02/05/2016.
+//  Copyright © 2016 Mathieu Vandeginste. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    
+    private var accumulator = 0.0
+    private var lastOperation = ""
+    private var internalProgram = [AnyObject]()
+    
+    func setOperand(operand: Double){
+        accumulator = operand
+        internalProgram.append(operand)
+    }
+    
+    func performOperation(symbol: String){
+        internalProgram.append(symbol)
+        if let operation = operations[symbol] {
+            lastOperation = ""
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator, symbol: symbol)
+            case .Equals:
+                executePendingBinaryOperation()
+            case .Clear :
+                accumulator = 0
+                pending = nil
+            case .Percent :
+                if pending == nil {
+                    accumulator = accumulator / 100
+                } else {
+                    accumulator = accumulator / 100 *  pending!.firstOperand
+                }
+            }
+        }
+    }
+    
+    var result: Double {
+        return accumulator
+    }
+    
+    var history: String {
+        return lastOperation
+    }
+    
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+        case Clear
+        case Percent
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double,Double) -> Double
+        var firstOperand: Double
+        var symbol: String
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            lastOperation = "\(pending!.firstOperand) \(pending!.symbol) \(accumulator) = "
+            accumulator = pending!.binaryFunction(pending!.firstOperand,accumulator)
+            pending = nil
+        }
+    }
+    
+    private var operations = [
+        //Constants
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        //Clear
+        "C" : Operation.Clear,
+        //Unary operations
+        "cos" : Operation.UnaryOperation(cos),
+        "sin" : Operation.UnaryOperation(sin),
+        "tan" : Operation.UnaryOperation(tan),
+        "√" : Operation.UnaryOperation(sqrt),
+        "±" :Operation.UnaryOperation({-$0}),
+        //Binary operations
+        "+" : Operation.BinaryOperation({$0 + $1}),
+        "-" : Operation.BinaryOperation({$0 - $1}),
+        "×" : Operation.BinaryOperation({$0 * $1}),
+        "÷" : Operation.BinaryOperation({$0 / $1}),
+        //Equals
+        "=" : Operation.Equals,
+        //Percent
+        "%" : Operation.Percent
+    ]
+    
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList {
+        get {
+            return internalProgram
+        }
+        set {
+            clear()
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let symbol = op as? String {
+                        performOperation(symbol)
+                    } else if let operand = op as? Double {
+                        setOperand(operand)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func clear() {
+        internalProgram.removeAll()
+        accumulator = 0.0
+        pending = nil
+    }
+    
+    
+}
---
+++ Calculator/CalculatorViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mathieu Vandeginste on 02/05/2016.
+//  Copyright © 2016 Mathieu Vandeginste. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    private var isInTheMiddleOfTyping = false
+    
+    private var brain = CalculatorBrain()
+    
+    private var screenValue: Double {
+        get {
+            return Double(self.screen.text!)!
+        }
+        set {
+            self.screen.text! = String(newValue)
+        }
+    }
+    
+    @IBOutlet weak private var screen: UILabel!
+    @IBOutlet weak var historyScreen: UILabel!
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        if self.isInTheMiddleOfTyping {
+            if sender.currentTitle! == "." {
+                if !self.screen.text!.containsString(".") {
+                    self.screen.text! += sender.currentTitle!
+                }
+            } else {
+                self.screen.text! += sender.currentTitle!
+            }
+        } else {
+            self.isInTheMiddleOfTyping = true
+            self.historyScreen.text! = ""
+            self.screen.text! = sender.currentTitle!
+        }
+    }
+    
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if self.isInTheMiddleOfTyping {
+            self.brain.setOperand(self.screenValue)
+            self.isInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            self.brain.performOperation(mathematicalSymbol)
+            self.screenValue = self.brain.result
+            self.historyScreen.text = self.brain.history
+        }
+    }
+    
+    
+    var savedProgram: CalculatorBrain.PropertyList?
+    @IBAction private func save() {
+        savedProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if let backup = savedProgram {
+            brain.program = backup
+            screenValue = brain.result
+        }
+    }
+    
+    @IBAction func clearMemory() {
+        savedProgram = nil
+    }
+    
+    
+}
+
